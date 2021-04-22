cd9c8bf8463d2a26c1e2ef51969bb77049266628
Tanggy91141 <63408716+Tanggy91141@users.noreply.github.com>
2021-04-22 07:36:26 +0700

    Delete the test part

--- Lap4interrupt_DMA/Core/Src/main.c
+++ Lap4interrupt_DMA/Core/Src/main.c
@@ -55,10 +55,8 @@ uint32_t Time_Find = 0;				//We have to find THIS ONE!!!
 
 uint32_t time_LEDoff = 0;
 uint32_t time_waitfor_LEDon = 0;
-uint32_t time_LEDon = 0;
-uint32_t time_runaway = 0;			//When user raise his/her finger.
-
-uint32_t time_test = 0;
+//uint32_t time_LEDon = 0;
+//uint32_t time_runaway = 0;			//When user raise his/her finger.
 
 /* USER CODE END PV */
 
